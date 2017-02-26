4f4baedf16eab99a8cce28c36aa79a5933ecbce1
Alex Rodriguez <arod2634@gmail.com>
2017-02-25 20:39:31 -0600

    Finishing Desktop and Mobile Navigation Menus

--- src/index.js
+++ src/index.js
@@ -3,26 +3,31 @@ import ReactDOM from 'react-dom';
 import BurgerMenu from 'react-burger-menu';
 import Headroom from 'react-headroom';
 import './styles/App.css';
-import logo from './assets/rise-float-community-gathering-logo-white.png';
+import logo from './assets/rise-logo-white.png';
 
 let App = React.createClass({
 
   getMobileMenu() {
     const Menu = BurgerMenu[this.state.currentMenu];
     const items = [
-      <a key="0" href=""><span>Schedule</span></a>,
-      <a key="1" href=""><span>Speakers</span></a>,
-      <a key="2" href=""><span>Details</span></a>,
-      <a key="3" href=""><span>Get Tickets</span></a>,
+      <span key="0" className="title is-3 bold">General Information</span>,
+      <a key="1" href="/schedule">Schedule</a>,
+      <a key="2" href="/speakers">Speakers</a>,
+      <a key="3" href="/details">Details</a>,
       <br key="4"/>,
-      <a key="5" href=""><span>Resources</span></a>,
-      <a key="6" href=""><span>About Rise</span></a>,
-      <a key="7" href=""><span>2017 Volunteers</span></a>,
-      <a key="8" href=""><span>2017 Vendors & Sponsors</span></a>,
-      <a key="9" href=""><span>Float STL</span></a>,
-      <a key="10" href=""><span>Meet the Team</span></a>,
-      <br key="11"/>,
-      <a key="12" href=""><span>Connect With Us</span></a>,
+      <a key="5" className="button is-large" href="https://rise.bazaarpass.com/products/2017">Get Tickets</a>,
+      <br key="6"/>,
+      <hr key="6"/>,
+      <br key="7"/>,
+      <span key="8" className="title is-4 bold">Resources</span>,
+      <a key="9" style={{fontSize: '1.2rem'}} href=""><span>About Rise</span></a>,
+      <a key="10" style={{fontSize: '1.2rem'}} href=""><span>Volunteers</span></a>,
+      <a key="11" style={{fontSize: '1.2rem'}} href=""><span>Vendors & Sponsors</span></a>,
+      <a key="12" style={{fontSize: '1.2rem'}} href=""><span>Float STL</span></a>,
+      <a key="13" style={{fontSize: '1.2rem'}} href=""><span>Meet the Team</span></a>,
+      <hr key="6"/>,
+      <a key="15" href="/"><span className="title is-4 bold">Connect With Us</span></a>,
+      <span key="18" className="icon"><i className="fa fa-facebook" aria-hidden="true"></i></span>,
     ]
 
     let MenuWrap = React.createClass({
@@ -94,38 +99,56 @@ let App = React.createClass({
   getInitialState() {
     return {
       currentMenu: 'slide',
-      side: 'right'
+      side: 'right',
+      navColor: 'transparent',
     };
+  },
+
+  componentDidMount: function() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+
+  componentWillUnmount: function() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+
+  handleScroll: function(event) {
+    if(window.pageYOffset >= 500) {
+      this.setState({
+        navColor: 'solid'
+      });
+    } else {
+      this.setState({
+        navColor: 'transparent'
+      });
+    }
   },
 
   render() {
     return (
       <div id="outer-container">
         {this.getMobileMenu()}
-        <Headroom>
-          <nav className="nav">
+        <section className="hero home">
+        <Headroom onScroll={this.handleScroll}>
+          <nav className={this.state.navColor + ' nav'}>
             <div className="container">
               <div className="columns">
-                <div className="column is-one-fifth">
+                <div className="column is-one-fourth logo">
                   <a href="/"><img src={logo} alt="Rise - A Float Community Gathering Logo White" /></a>
                 </div>
-                <div className="column is-one-fifth menu-item">
+                <div className="column is-one-fourth menu-item">
                   <a href="/schedule/">Schedule</a>
-               </div>
-               <div className="column is-one-fifth menu-item">
-                <a href="/speakers/">Speakers</a>
+                </div>
+                <div className="column is-one-fourth menu-item">
+                  <a href="/speakers/">Speakers</a>
+                </div>
+               <div className="column is-one-fourth menu-item">
+                <a className="button is-large" href="https://rise.bazaarpass.com/products/2017">Buy Your Tickets</a>
               </div>
-              <div className="column is-one-fifth menu-item">
-               <a href="/details/">Details</a>
-             </div>
-             <div className="column is-one-fifth menu-item">
-              <a href="https://rise.bazaarpass.com/products/2017">Buy Your Tickes</a>
-            </div>
               </div>
             </div>
           </nav>
         </Headroom>
-        <section className="hero">
           <div className="hero-body">
             <div className="container">
               <h1 className="title">
