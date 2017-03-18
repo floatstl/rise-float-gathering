a1afb19972af6fc38fd7e905ce4da0981bee9ebb
Alex Rodriguez <arod2634@gmail.com>
2017-03-17 22:02:30 -0500

    Updating About Page

--- src/components/About.js
+++ src/components/About.js
@@ -5,14 +5,17 @@ import ParticleParams from './../particles.json';
 import Header from './Header';
 import Footer from './Footer';
 
+import FloatLogo from '../assets/floatstl.png'
+
 let About = React.createClass({
   render() {
     return (
       <div id="outer-container">
-        <section className="hero home">
+        <section className="hero home about-page">
           <Header isHome={true}/>
           <div className="hero-body">
             <div className="container">
+              <div className="has-text-centered" ><img src={FloatLogo} alt="Float STL Logo"/></div><br/><br/>
               <h1 className="title is-1 has-text-centered">About Rise</h1><br/>
               <p>The float is sacred. The float is powerful. The float is joyful. Throughout the many recognized benefits of a floating practice, there is an underlying current of complexity and spirituality. Our early experiences with floating helped us to connect with this sense of clarity, wonder, peace, and stillness.</p>
               <br/>
@@ -23,7 +26,7 @@ let About = React.createClass({
               <p>Rise: Float Community Gathering is a place to connect to The Heart of Floating. Rise is an outgrowth of our desire to create, grow, maintain, and strengthen those bonds. It is a place to meet, gather, and celebrate. We have learned that sharing experiences and coming together in a shared space helps to develop and strengthen community. The purpose of Rise is to highlight these deeply held values and hold space for community integration. Looking forward, the healthy growth of this industry can only occur if we collectively honor and feed its roots.</p>
               <br/>
               <p>We are just getting started. This is just the beginning. Our goal for this first event is to invite and welcome individuals who have played an impactful role in our journey. We hold deep gratitude and respect for their contributions and are honored to have them here in Saint Louis for this float community event.</p>
-              <br/><br/><div className="title is-1 has-text-centered"><span className="icon"><a href="https://www.facebook.com/events/1901297480100411/" target="_blank"><i className="fa fa-facebook" style={{'color': 'white'}} aria-hidden="true"></i></a></span></div>
+              <br/><br/><br/><div className="title is-1 has-text-centered"><span className="icon"><a href="https://www.facebook.com/events/1901297480100411/" target="_blank"><i className="fa fa-facebook" style={{'color': 'white'}} aria-hidden="true"></i></a></span></div>
             </div>
           </div>
         </section>
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import AddToCalendar from 'react-add-to-calendar';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
 
 import Header from './Header';
 import Footer from './Footer';
@@ -8,11 +10,105 @@ import ConnectImage from '../assets/connect.png';
 import DiscoverImage from '../assets/discover.png';
 import Refresh from '../assets/refresh.png';
 
+BigCalendar.setLocalizer(
+  BigCalendar.momentLocalizer(moment)
+);
+
 let Schedule = React.createClass({
     render() {
       return (
         <div id="outer-container">
           <Header/>
+          <section className="section schedule-details">
+            <div className="container has-text-centered">
+              <h3 className="title is-2 has-text-centered">Schedule</h3>
+              <p className="has-text-centered">Coming Soon!</p><br/><br/>
+              <BigCalendar
+                 {...this.props}
+                 events={[
+                  {
+                    'title': 'All Day Event',
+                    'allDay': true,
+                    'start': new Date(2015, 3, 0),
+                    'end': new Date(2015, 3, 1)
+                  },
+                  {
+                    'title': 'Long Event',
+                    'start': new Date(2015, 3, 7),
+                    'end': new Date(2015, 3, 10)
+                  },
+
+                  {
+                    'title': 'DTS STARTS',
+                    'start': new Date(2016, 2, 13, 0, 0, 0),
+                    'end': new Date(2016, 2, 20, 0, 0, 0)
+                  },
+
+                  {
+                    'title': 'DTS ENDS',
+                    'start': new Date(2016, 10, 6, 0, 0, 0),
+                    'end': new Date(2016, 10, 13, 0, 0, 0)
+                  },
+
+                  {
+                    'title': 'Some Event',
+                    'start': new Date(2015, 3, 9, 0, 0, 0),
+                    'end': new Date(2015, 3, 9, 0, 0, 0)
+                  },
+                  {
+                    'title': 'Conference',
+                    'start': new Date(2015, 3, 11),
+                    'end': new Date(2015, 3, 13),
+                    desc: 'Big conference for important people'
+                  },
+                  {
+                    'title': 'Meeting',
+                    'start': new Date(2015, 3, 12, 10, 30, 0, 0),
+                    'end': new Date(2015, 3, 12, 12, 30, 0, 0),
+                    desc: 'Pre-meeting meeting, to prepare for the meeting'
+                  },
+                  {
+                    'title': 'Lunch',
+                    'start':new Date(2015, 3, 12, 12, 0, 0, 0),
+                    'end': new Date(2015, 3, 12, 13, 0, 0, 0),
+                    desc: 'Power lunch'
+                  },
+                  {
+                    'title': 'Meeting',
+                    'start':new Date(2015, 3, 12,14, 0, 0, 0),
+                    'end': new Date(2015, 3, 12,15, 0, 0, 0)
+                  },
+                  {
+                    'title': 'Happy Hour',
+                    'start':new Date(2015, 3, 12, 17, 0, 0, 0),
+                    'end': new Date(2015, 3, 12, 17, 30, 0, 0),
+                    desc: 'Most important meal of the day'
+                  },
+                  {
+                    'title': 'Dinner',
+                    'start':new Date(2015, 3, 12, 20, 0, 0, 0),
+                    'end': new Date(2015, 3, 12, 21, 0, 0, 0)
+                  },
+                  {
+                    'title': 'Birthday Party',
+                    'start':new Date(2015, 3, 13, 7, 0, 0),
+                    'end': new Date(2015, 3, 13, 10, 30, 0)
+                  }
+                ]}
+                 defaultDate={new Date(2015, 3, 1)}
+               />
+              <AddToCalendar
+                event={{
+                  title: 'Rise: Float Community Gathering',
+                  description: 'Rise is a gathering of float industry enthusiasts and professionals to connect, discover and relax. Learn more and purchase tickets at http://riserfloatgathering.com',
+                  location: '4240 Duncan Ave St. Louis, MO 63110',
+                  startTime: '2017-04-28T19:00:00-04:00',
+                  endTime: '2017-04-30T19:00:00-04:00'
+                }}
+                buttonTemplate={{ 'calendar-plus-o': 'left' }}
+                className="has-text-centered"/><br/>
+            </div>
+          </section>
           <section className="section schedule">
             <div className="container">
               <h3 className="title is-2 has-text-centered">What to Expect</h3>
@@ -40,22 +136,6 @@ let Schedule = React.createClass({
                   </div>
                 </div>
               </div>
-            </div>
-          </section>
-          <section className="section schedule-details">
-            <div className="container has-text-centered">
-              <h3 className="title is-2 has-text-centered">Schedule</h3>
-              <p className="has-text-centered">Coming Soon!</p><br/><br/>
-              <AddToCalendar
-                event={{
-                  title: 'Rise: Float Community Gathering',
-                  description: 'Rise is a gathering of float industry enthusiasts and professionals to connect, discover and relax. Learn more and purchase tickets at http://riserfloatgathering.com',
-                  location: '4240 Duncan Ave St. Louis, MO 63110',
-                  startTime: '2017-04-28T19:00:00-04:00',
-                  endTime: '2017-04-30T19:00:00-04:00'
-                }}
-                buttonTemplate={{ 'calendar-plus-o': 'left' }}
-                className="has-text-centered"/><br/>
             </div>
           </section>
         <Footer/>
